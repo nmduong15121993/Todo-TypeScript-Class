e175331ff2bd3f9dff893b28d0073a4d9deebbe6
Duong Nguyen <nmduong15121993@gmail.com>
2021-07-18 23:23:37 +0700

    update format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import { InputTodo } from './InputTodo';
-import { TTodos } from './type';
-import { ListTodo } from './ListTodo';
+import * as React from "react";
+import { InputTodo } from "./InputTodo";
+import { TTodos } from "./type";
+import { ListTodo } from "./ListTodo";
 
 type TState = {
-  todos: TTodos[],
+  todos: TTodos[];
 };
 
 class App extends React.Component<{}, TState> {
@@ -14,64 +14,58 @@ class App extends React.Component<{}, TState> {
       todos: [],
     };
     this.onAddTodo = this.onAddTodo.bind(this);
-  };
+  }
 
   setTodos(todos: TTodos[]) {
-    this.setState({todos});
-  };
+    this.setState({ todos });
+  }
 
   onAddTodo(newInput: string) {
     let { todos } = this.state;
-    todos.push(
-      {
-        content: newInput,
-        state: false,
-      },
-    );
+    todos.push({
+      content: newInput,
+      state: false,
+    });
     this.setTodos(todos);
-  };
+  }
 
   onChangeState(newState: boolean, index: number) {
     const todoClone = [...this.state.todos];
     todoClone[index].state = newState;
     this.setTodos(todoClone);
-  };
+  }
 
   onDelete(index: number) {
     let todoClone = [...this.state.todos];
     todoClone.splice(index, 1);
     this.setTodos(todoClone);
-  };
+  }
 
   onEdit(newTodo: string, index: number) {
-    console.log({edit: newTodo})
+    console.log({ edit: newTodo });
     let todoClone = [...this.state.todos];
     todoClone[index].content = newTodo;
     this.setTodos(todoClone);
-  };
+  }
 
   render() {
     const { todos } = this.state;
 
     return (
       <>
-        <InputTodo 
-          onAddTodo={this.onAddTodo}
-        />
-        {
-          todos.map((item, index) => (
-            <ListTodo 
-              key={`${item}-${index}`}
-              todos={item}
-              onChangeState={(newState) => this.onChangeState(newState, index)}
-              onDelete={() => this.onDelete(index)}
-              onEdit={(newTodo) => this.onEdit(newTodo, index)}
-            />
-          ))
-        }
+        <InputTodo onAddTodo={this.onAddTodo} />
+        {todos.map((item, index) => (
+          <ListTodo
+            key={`${item}-${index}`}
+            todos={item}
+            onChangeState={(newState) => this.onChangeState(newState, index)}
+            onDelete={() => this.onDelete(index)}
+            onEdit={(newTodo) => this.onEdit(newTodo, index)}
+          />
+        ))}
       </>
     );
-  };
-};
+  }
+}
 
 export { App };
--- src/InputTodo.tsx
+++ src/InputTodo.tsx
@@ -1,54 +1,50 @@
-import * as React from 'react';
+import * as React from "react";
 
 type TProps = {
-  onAddTodo: (input: string) => void,
+  onAddTodo: (input: string) => void;
 };
 
 type TState = {
-  input: string,
+  input: string;
 };
 
 class InputTodo extends React.Component<TProps, TState> {
   constructor(props: TProps) {
     super(props);
     this.state = {
-      input: '',
+      input: "",
     };
     this.onAddTodoFn = this.onAddTodoFn.bind(this);
-
-  };
+  }
 
   setInput(input: string) {
     this.setState({ input });
-  };
+  }
 
   onChangeFn(target: EventTarget & HTMLInputElement) {
     this.setInput(target.value);
-  };
+  }
 
   onAddTodoFn() {
     const { input } = this.state;
     this.props.onAddTodo(input);
-    this.setInput('');
-  };
+    this.setInput("");
+  }
 
   render() {
     return (
       <>
         <input
-          type='text' 
+          type="text"
           value={this.state.input}
           onChange={({ target }) => this.onChangeFn(target)}
         />
-        <button
-          type='button'
-          onClick={this.onAddTodoFn}
-        >
+        <button type="button" onClick={this.onAddTodoFn}>
           Add
         </button>
       </>
     );
-  };
-};
+  }
+}
 
 export { InputTodo };
